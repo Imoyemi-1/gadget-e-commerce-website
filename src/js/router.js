bdcd28bc6e182e4ab86c4f1f0f9eb7a6c3005273
Imoyemi <okeibrahim133@gmail.com>
2025-08-15 20:31:04 -0500

    feat: add color and underline menu

--- src/js/router.js
+++ src/js/router.js
@@ -15,10 +15,34 @@ const routes = {
 };
 
 const handleLocation = async () => {
+  const link = document.querySelectorAll('#menu > a');
   const path = window.location.pathname;
   const route = routes[path] || routes[404];
 
   const html = await fetch(route).then((data) => data.text());
+
+  [...link].forEach((item) => {
+    item.classList.remove('text-blue-400');
+    item.classList.remove('underline');
+  });
+  switch (path) {
+    case '/':
+      link[0].classList.add('text-blue-400'),
+        link[0].classList.add('underline');
+      break;
+    case '/shop':
+      link[1].classList.add('text-blue-400'),
+        link[1].classList.add('underline');
+      break;
+    case '/about':
+      link[2].classList.add('text-blue-400'),
+        link[2].classList.add('underline');
+      break;
+    default:
+      link[3].classList.add('text-blue-400'),
+        link[3].classList.add('underline');
+      break;
+  }
   document.querySelector('#main-page').innerHTML = html;
 };
 
